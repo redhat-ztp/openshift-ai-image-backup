f1d0c984fc24464f8e5528770a924a391b1e0452
Daniel Mellado <dmellado@redhat.com>
2021-10-13 16:15:48 +0200

    Refactor the structure
    This PR refactors the repo structure and imports.

--- Makefile
+++ Makefile
@@ -1,18 +1,34 @@
+export GO111MODULE=on
+unexport GOPATH
+
 CONTAINER_COMMAND = $(shell if [ -x "$(shell which podman)" ];then echo "podman" ; else echo "docker";fi)
 IMAGE := $(or ${IMAGE},quay.io/yrobla/openshift-ai-image-backup:latest)
 GIT_REVISION := $(shell git rev-parse HEAD)
 CONTAINER_BUILD_PARAMS = --label git_revision=${GIT_REVISION}
 
 all: build build-image push-image
+.PHONY: all
+
+# Include the library makefile
+include $(addprefix ./vendor/github.com/openshift/build-machinery-go/make/, \
+	golang.mk \
+	targets/openshift/deps-gomod.mk \
+    targets/openshift/images.mk \
+)
 
 build:
-	CGO_ENABLED=0 go build -o build/openshift-ai-image-backup src/main.go
+	CGO_ENABLED=0 go build -o build/openshift-ai-image-backup pkg/main.go
+.PHONY: build
 
 build-image:
 	$(CONTAINER_COMMAND) build $(CONTAINER_BUILD_PARAMS) -f Dockerfile . -t $(IMAGE)
+.PHONY:
 
 push-image:
 	$(CONTAINER_COMMAND) push ${IMAGE}
 
 clean:
 	rm -rf build
+.PHONY: build
+
+GO_TEST_PACKAGES :=./pkg/... ./cmd/...
---
+++ cmd/backupReleaseImage.go
@@ -0,0 +1,106 @@
+/*
+Copyright © 2021 Yolanda Robla <yroblamo@redhat.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// sync image with skopeo
+func syncImage(ReleaseImageURL string, BackupPath string, AuthFilePath string) error {
+	// get the file name from the image to check if exists
+	filename := ReleaseImageURL[strings.LastIndex(ReleaseImageURL, "/")+1:]
+	if _, err := os.Stat(fmt.Sprintf("%s/%s", BackupPath, filename)); os.IsNotExist(err) {
+		// first remove all content in that folder
+		os.RemoveAll(BackupPath)
+
+		skopeoArgs := []string{"sync", "--src", "docker", "--dest", "dir", ReleaseImageURL, BackupPath}
+
+		if len(AuthFilePath) > 0 {
+			// add the authfile flag
+			skopeoArgs = append(skopeoArgs, []string{"--authfile", AuthFilePath}...)
+		}
+
+		_, err := exec.Command("/usr/bin/skopeo", skopeoArgs...).Output()
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+		log.Info(fmt.Sprintf("Sync completed for %s into %s", ReleaseImageURL, BackupPath))
+		return nil
+	} else {
+		log.Info(fmt.Sprintf("Image %s already exists, skipping download", filename))
+		return nil
+	}
+}
+
+var backupReleaseImageCmd = &cobra.Command{
+	Use:   "backupReleaseImage",
+	Short: "It will sync the release image specified into local path specified",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ReleaseImageURL, _ := cmd.Flags().GetString("ReleaseImageURL")
+		BackupPath, _ := cmd.Flags().GetString("BackupPath")
+
+		// validate url
+		_, err := url.ParseRequestURI("docker://"+ ReleaseImageURL)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+		
+		// validate path
+		if _, err := os.Stat(BackupPath); os.IsNotExist(err) {
+			// create path
+			err := os.Mkdir(BackupPath, os.ModePerm)
+			if err != nil {
+				log.Error(err)
+				return err
+			}
+		}
+
+		// start syncing image
+		AuthFilePath := ""
+		if cmd.Flags().Changed("AuthFilePath") {
+			AuthFilePath, _ = cmd.Flags().GetString("AuthFilePath")
+		}
+		return syncImage(ReleaseImageURL, BackupPath, AuthFilePath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(backupReleaseImageCmd)
+
+	backupReleaseImageCmd.Flags().StringP("ReleaseImageURL", "r", "", "URL for the release image to sync")
+	backupReleaseImageCmd.Flags().StringP("BackupPath", "p", "", "Path where to sync the release image")
+	backupReleaseImageCmd.Flags().StringP("AuthFilePath", "a", "", "Path to the registry authentication file")
+	backupReleaseImageCmd.MarkFlagRequired("ReleaseImageURL")
+	backupReleaseImageCmd.MarkFlagRequired("BackupPath")
+
+	// bind to viper
+	viper.BindPFlag("ReleaseImageURL", backupReleaseImageCmd.Flags().Lookup("ReleaseImageURL"))
+	viper.BindPFlag("BackupPath", backupReleaseImageCmd.Flags().Lookup("BackupPath"))
+	viper.BindPFlag("AuthFilePath", backupReleaseImageCmd.Flags().Lookup("AuthFilePath"))
+}
