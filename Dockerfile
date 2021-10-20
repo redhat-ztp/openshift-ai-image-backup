941c6c8d5fadb81b65b4f77092a210872ae41114
Sabbir Hasan <sahasan@sahasan.remote.csb>
2021-10-20 02:28:34 +0200

    application image backup

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
 FROM registry.ci.openshift.org/openshift/release:golang-1.16 AS builder
 ENV GOFLAGS=-mod=mod
+ENV VERSION="v1.22.0"
 WORKDIR /go/src/github.com/redhat-ztp/openshift-ai-image-backup
 
-RUN yum -y install skopeo
+RUN yum -y install skopeo && yum -y install wget
 RUN useradd skopeo
 
 # Setup skopeo's uid/guid entries
 RUN echo skopeo:100000:65536 > /etc/subuid
 RUN echo skopeo:100000:65536 > /etc/subgid
+
+# install crictl
+RUN wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz \
+         && tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C . \
+         && rm -f crictl-$VERSION-linux-amd64.tar.gz
+
 
 # Bring in the go dependencies before anything else so we can take
 # advantage of caching these layers in future builds.
@@ -21,6 +28,10 @@ RUN make build
 FROM quay.io/centos/centos:stream8
 
 COPY --from=builder /go/src/github.com/redhat-ztp/openshift-ai-image-backup/bin/openshift-ai-image-backup /usr/bin/openshift-ai-image-backup
+COPY --from=builder /go/src/github.com/redhat-ztp/openshift-ai-image-backup/crictl /usr/bin/crictl
+
+# install jq
+RUN yum -y install jq
 
 # Point to the Authorization file
 ENV REGISTRY_AUTH_FILE=/tmp/auth.json
---
+++ cmd/backupApplicationImage.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func copyImage(BackupPath string) error {
+	//bash command to execute on the spoke
+	bashArgs := fmt.Sprintf("for id in $(crictl images -o json | jq -r '.images[].id'); skopeo copy containers-storage:$id dir:%s/$id; done", BackupPath)
+
+	_, err := exec.Command(bashArgs).Output()
+	if err != nil {
+		log.Errorf("Couldn't execute the command, err: %s", err)
+		return err
+	}
+
+	log.Info(fmt.Sprintf("Application images have been copied into %s", BackupPath))
+	return nil
+
+}
+
+var backupApplicationImageCmd = &cobra.Command{
+	Use:   "backupApplicationImage",
+	Short: "It will copy the application images available under /var/lib/containers into local path specified",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		BackupPath, _ := cmd.Flags().GetString("BackupPath")
+
+		// validate path
+		if _, err := os.Stat(BackupPath); os.IsNotExist(err) {
+			// create path
+			err := os.Mkdir(BackupPath, os.ModePerm)
+			if err != nil {
+				log.Error(err)
+				return err
+			}
+		}
+
+		return copyImage(BackupPath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(backupApplicationImageCmd)
+
+	backupApplicationImageCmd.Flags().StringP("BackupPath", "p", "", "Path where to copy the application images")
+	backupApplicationImageCmd.MarkFlagRequired("BackupPath")
+
+	// bind to viper
+	viper.BindPFlag("BackupPath", backupApplicationImageCmd.Flags().Lookup("BackupPath"))
+
+}
