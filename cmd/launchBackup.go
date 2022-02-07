bd11e3c64f576fcaa359783d6636b7f6be57d6f0
Don Penney <dpenney@redhat.com>
2022-02-07 12:23:04 -0500

    Additional cleanup of recovery script
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- cmd/launchBackup.go
+++ cmd/launchBackup.go
@@ -73,7 +73,7 @@ func LaunchBackup(BackupPath string) error {
 	log.Info("Upgrade recovery script written")
 
 	// Take backup
-	backupCmd := fmt.Sprintf("%s --take-backup %s", scriptname, BackupPath)
+	backupCmd := fmt.Sprintf("%s --take-backup %s --skip-images", scriptname, BackupPath)
 	err = ExecuteCmd(backupCmd)
 	if err != nil {
 		return err
